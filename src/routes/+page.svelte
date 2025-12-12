72edac85815065674263825808c718cbfdcd702d
john <jrobdikeman@gmail.com>
2025-12-12 14:52:38 -0600

    animated picture thingies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,20 +6,38 @@
   import { ScrollSmoother } from "gsap/ScrollSmoother"
   import { ScrollToPlugin } from "gsap/ScrollToPlugin"
   import { TextPlugin } from "gsap/TextPlugin"
+  import { onMount } from "svelte"
 
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin, TextPlugin)
+
+  onMount(() => {
+    gsap
+      .to(".flip-in-left", {
+        rotation: 15,
+        left: "0%",
+        ease: "power3.out",
+        duration: 1,
+      })
+      .play()
+    gsap
+      .to(".flip-in-right", {
+        rotation: -15,
+        right: "0%",
+        ease: "power3.out",
+        duration: 1,
+      })
+      .play()
+  })
 </script>
 
 <div class="container">
-  <div class="color-block">
-    <h1>we are getting married :)</h1>
+  <div class="img-container flip-in-left">
+    <img src="{base}/photo1.jpg" />
   </div>
-  <img src="{base}/photo1.jpg" />
 </div>
 <div class="container">
-  <img src="{base}/photo2.jpg" />
-  <div class="color-block">
-    <h1>it will be described by a time, and a place in space.</h1>
+  <div class="img-container flip-in-right">
+    <img src="{base}/photo2.jpg" />
   </div>
 </div>
 
@@ -33,8 +51,6 @@
   :global(body) {
     font-family: "Cartograph-CF", sans-serif;
     font-size: 2em;
-    min-height: 100vh;
-    display: flex;
     align-items: center;
     justify-content: center;
     background: #f5f5f5;
@@ -43,79 +59,29 @@
     box-sizing: border-box;
   }
 
+  .img-container {
+    width: 30%;
+    aspect-ratio: 0.75;
+    position: relative;
+  }
+
+  .flip-in-left {
+    left: -30%;
+  }
+
+  .flip-in-right {
+    float: right;
+    right: -30%;
+  }
+
+  .img-container img {
+    height: 100%;
+    width: auto;
+    object-fit: cover;
+  }
+
   .container {
-    display: flex;
     width: 100vw;
-    box-shadow: 0 10px 40px rgba(0, 0, 0, 0.1);
-  }
-
-  .color-block {
-    flex: 1;
-    background: linear-gradient(135deg, #ff6961 0%, #ffc5d3 100%);
-    padding: 60px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    color: white;
-  }
-
-  .color-block h1 {
-    font-size: 48px;
-    margin-bottom: 20px;
-    font-weight: bold;
-  }
-
-  .color-block p {
-    font-size: 18px;
-    line-height: 1.6;
-    margin-bottom: 30px;
-  }
-
-  .color-block button {
-    background: white;
-    color: #667eea;
-    border: none;
-    padding: 15px 30px;
-    font-size: 16px;
-    font-weight: bold;
-    border-radius: 5px;
-    cursor: pointer;
-    width: fit-content;
-    transition: transform 0.2s;
-  }
-
-  .color-block button:hover {
-    transform: translateY(-2px);
-  }
-
-  .photo-side-1 {
-    flex: 1;
-    background: url("/photo1.jpg");
-    /*min-height: 729.6px;*/
-    background-size: contain;
-    background-repeat: no-repeat;
-  }
-
-  img {
-    flex: 1;
-    max-width: 50vw;
-  }
-
-  .photo-side-2 {
-    flex: 1;
-    background: url("/photo2.jpg");
-    /*min-height: 816px;*/
-    background-size: contain;
-    background-repeat: no-repeat;
-  }
-
-  @media (max-width: 768px) {
-    .container {
-      flex-direction: column;
-    }
-
-    .photo-side {
-      min-height: 1000px;
-    }
+    height: 100vh;
   }
 </style>
