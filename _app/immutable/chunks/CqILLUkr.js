cd0889d0afc858adfd39554e06360c83fff279f7
john <jrobdikeman@gmail.com>
2026-01-10 18:25:30 -0600

    Updates

--- _app/immutable/chunks/CqILLUkr.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1fv9doa?.base??"/sveltekit-gh-pages",a=globalThis.__sveltekit_1fv9doa?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DhtF7V77.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_mhoduc?.base??"/wedsite",e=globalThis.__sveltekit_mhoduc?.assets??s??"";export{e as a,s as b};
