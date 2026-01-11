12479b9acccfff8f97fa7b5dc77789f98cd913fd
john <jrobdikeman@gmail.com>
2026-01-10 18:24:08 -0600

    fix url path

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-static"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+      base: process.env.NODE_ENV === "production" ? "/wedsite" : "",
     },
   },
-};
+}
 
-export default config;
+export default config
